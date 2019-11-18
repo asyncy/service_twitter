a687a2dabaf5d6c84cb6d026203942668b571c25
matthewhudson <matthud@gmail.com>
2019-11-18 16:04:59 -0500

    style(app): format all Python code according to Black

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ from stream import Stream
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+
 
 def getOauth():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -26,10 +27,10 @@ def getOauth():
 
 def body():
     data = request.json
-    if data.get('handle'):
-         data['screen_name'] = data.pop('handle')
-    if data.get('user'):
-         data['user_id'] = data.pop('user')
+    if data.get("handle"):
+        data["screen_name"] = data.pop("handle")
+    if data.get("user"):
+        data["user_id"] = data.pop("user")
     return data
 
 
@@ -40,108 +41,105 @@ def response(res):
     return dict(res)
 
 
-@app.route('/tweet', methods=['POST'])
+@app.route("/tweet", methods=["POST"])
 def tweet():
     t = getOauth()
     data = request.json
-    if data.get('status') is None or data.get('status') == "":
+    if data.get("status") is None or data.get("status") == "":
         res = {
-            'status': 'failed',
-            'reason': "'status' parameter is missing",
+            "status": "failed",
+            "reason": "'status' parameter is missing",
         }
         response_code = 400
         return dumps(res), response_code
     return dumps(t.update_status(data.pop("status"))._json)
 
 
-@app.route('/retweet', methods=['POST'])
+@app.route("/retweet", methods=["POST"])
 def retweet():
     t = getOauth()
     data = request.json
-    if data.get('tweet') is None or data.get('tweet') == "":
-        res = {
-            'status': 'failed',
-            'reason': "'tweet' parameter is missing"
-        }
+    if data.get("tweet") is None or data.get("tweet") == "":
+        res = {"status": "failed", "reason": "'tweet' parameter is missing"}
         response_code = 400
         return dumps(res), response_code
     response = t.retweet(int(data.pop("tweet")))
     return dumps(response._json)
 
 
-@app.route('/follow', methods=['POST'])
+@app.route("/follow", methods=["POST"])
 def follow():
     t = getOauth()
     data = body()
-    if data.get('id') is not None and data.get('id') != "":
-        return dumps(t.create_friendship(data.pop("id"),follow=True)._json)
-    elif data.get('screen_name') is not None and data.get('screen_name') != "":
-        return dumps(t.create_friendship(data.pop("screen_name"),follow=True)._json)
+    if data.get("id") is not None and data.get("id") != "":
+        return dumps(t.create_friendship(data.pop("id"), follow=True)._json)
+    elif data.get("screen_name") is not None and data.get("screen_name") != "":
+        return dumps(t.create_friendship(data.pop("screen_name"), follow=True)._json)
     else:
         res = {
-            'status': 'failed',
-            'reason': "At least one of 'id' or 'screen_name' must be specified."
+            "status": "failed",
+            "reason": "At least one of 'id' or 'screen_name' must be specified.",
         }
         response_code = 400
         return dumps(res), response_code
 
 
-@app.route('/unfollow', methods=['POST'])
+@app.route("/unfollow", methods=["POST"])
 def unfollow():
     t = getOauth()
     data = body()
-    if data.get('id') is not None:
+    if data.get("id") is not None:
         return dumps(t.destroy_friendship(data.pop("id"))._json)
-    elif data.get('screen_name') is not None:
+    elif data.get("screen_name") is not None:
         return dumps(t.destroy_friendship(data.pop("screen_name"))._json)
     else:
         res = {
-            'status': 'failed',
-            'reason': "At least one of 'id' or 'screen_name' must be specified."
+            "status": "failed",
+            "reason": "At least one of 'id' or 'screen_name' must be specified.",
         }
         response_code = 400
         return dumps(res), response_code
 
-@app.route('/followers', methods=['POST'])
+
+@app.route("/followers", methods=["POST"])
 def followers():
     t = getOauth()
     data = body()
-    if ((data.get('user_id') is None or data.get('user_id') == "") 
-        and (data.get('screen_name') is None or data.get('screen_name') == "")):
+    if (data.get("user_id") is None or data.get("user_id") == "") and (
+        data.get("screen_name") is None or data.get("screen_name") == ""
+    ):
         res = {
-            'status': 'failed',
-            'reason': "At least one of 'handle' or 'user' must be specified."
+            "status": "failed",
+            "reason": "At least one of 'handle' or 'user' must be specified.",
         }
         response_code = 400
         return dumps(res), response_code
 
-    if data.get('user_id') is None or data.get('user_id') == "":
+    if data.get("user_id") is None or data.get("user_id") == "":
         responseArray = t.followers(data.get("user_id"))
-    if data.get('screen_name') is None or data.get('screen_name') == "":
+    if data.get("screen_name") is None or data.get("screen_name") == "":
         responseArray = t.followers(data.get("screen_name"))
-    
+
     resultArray = []
     for i in range(len(responseArray)):
         resultArray.append(responseArray[i]._json)
     return dumps(resultArray)
 
+
 subscriptions = {}
 
 
-@app.route('/stream/subscribe', methods=['POST'])
+@app.route("/stream/subscribe", methods=["POST"])
 def stream_subscribe():
     auth = OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
     data = body()
     if data["data"].get("track") is None or data["data"].get("track") == "":
-        res = {
-            'status': 'failed',
-            'reason': "'track' parameter is missing"
-        }
+        res = {"status": "failed", "reason": "'track' parameter is missing"}
         response_code = 400
         return dumps(res), response_code
-    subscriptions.setdefault(data['id'], Stream(auth, data).start())
-    return 'Subscribed'
+    subscriptions.setdefault(data["id"], Stream(auth, data).start())
+    return "Subscribed"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
--- stream.py
+++ stream.py
@@ -10,15 +10,17 @@ class Stream(threading.Thread):
         threading.Thread.__init__(self)
         stream = TwitterStream(auth=auth, block=True)
         self.stopped = False
-        if subscription['data'].get('isTesting') is not None and subscription['data'].get('isTesting') != "" :
-            self.testing = subscription['data'].get('isTesting')
+        if (
+            subscription["data"].get("isTesting") is not None
+            and subscription["data"].get("isTesting") != ""
+        ):
+            self.testing = subscription["data"].get("isTesting")
         else:
             self.testing = False
-        self.endpoint = subscription['endpoint']
+        self.endpoint = subscription["endpoint"]
 
-        if subscription['data'].get('track'):
-            self.iterator = stream.statuses.filter(
-                track=subscription['data']['track'])
+        if subscription["data"].get("track"):
+            self.iterator = stream.statuses.filter(track=subscription["data"]["track"])
         else:
             self.iterator = stream.statuses.sample()
 
@@ -30,28 +32,30 @@ class Stream(threading.Thread):
             if self.stopped:
                 raise StopIteration
             elif tweet is None:
-                print('Stream: No output')
+                print("Stream: No output")
             elif tweet is Timeout:
-                print('Stream: Timeout')
+                print("Stream: Timeout")
             elif tweet is HeartbeatTimeout:
-                print('Stream: Heartbeat Timeout')
+                print("Stream: Heartbeat Timeout")
             elif tweet is Hangup:
-                print('Stream: Hangup')
-            elif tweet.get('text'):
+                print("Stream: Hangup")
+            elif tweet.get("text"):
                 requests.post(
                     self.endpoint,
-                    headers={'Content-Type': 'application/json'},
-                    data=dumps({
-                        'eventType': 'tweet',
-                        'cloudEventsVersion': '0.1',
-                        'contentType': 'application/vnd.omg.object+json',
-                        'eventID': tweet['id'],
-                        'data': tweet
-                    })
+                    headers={"Content-Type": "application/json"},
+                    data=dumps(
+                        {
+                            "eventType": "tweet",
+                            "cloudEventsVersion": "0.1",
+                            "contentType": "application/vnd.omg.object+json",
+                            "eventID": tweet["id"],
+                            "data": tweet,
+                        }
+                    ),
                 )
-                if self.testing is True :
+                if self.testing is True:
                     sys.exit()
             else:
-                print(f'Stream: Other {str(tweet)}')
-                if self.testing is True :
+                print(f"Stream: Other {str(tweet)}")
+                if self.testing is True:
                     sys.exit()
